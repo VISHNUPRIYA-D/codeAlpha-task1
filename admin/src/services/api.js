a61d0eaf534eace0ac26fd126027423787d6cfd7
VISHNU PRIYA <166928822+VISHNUPRIYA-D@users.noreply.github.com>
2026-08-04 21:28:45 +0530

    Update api.js

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const api = axios.create(
     {
-    baseURL : 'https://shopandbuy-backend.onrender.com/admin'
+    baseURL : 'import.meta.env.VITE_BACKEND_URL'
     }
 )
 
